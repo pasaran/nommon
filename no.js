54c5c743a6adc4419ab2e05a382f904befedaafa
Sergei Nikitin <nik.pasaran@gmail.com>
2012-08-27 17:08:55 +0400

    Переносим из noscript'а все common'овское сюда

--- no.js
+++ no.js
@@ -1,6 +1,10 @@
 var no = require('./lib/no.js');
 
+require('./lib/no.events.js');
 require('./lib/no.jpath.js');
+require('./lib/no.promise.js');
+require('./lib/no.future.js');
+require('./lib/no.path.js');
 
 module.exports = no;
 
