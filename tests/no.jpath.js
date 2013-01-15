0ba92894a69acb4fb07435f3fdfc08856f037d76
Sergei Nikitin <nik.pasaran@gmail.com>
2013-01-15 18:57:06 +0400

    Тесты для #8

--- tests/no.jpath.js
+++ tests/no.jpath.js
@@ -220,6 +220,14 @@ describe('compare nodeset to nodeset', function() {
         select('.item[ .id == /.id ].id', data).should.be.eql( [ 'two' ] );
     });
 
+    it('.ids1 == .ids2', function() {
+        select('.ids1 == .ids2', data).should.be.not.ok;
+    });
+
+    it('.ids2 == .ids3', function() {
+        select('.ids2 == .ids3', data).should.be.ok;
+    });
+
 });
 
 //  ---------------------------------------------------------------------------------------------------------------  //
