ee69b22b6ae5b7729c89c4551385adb58f5b11f2
chestozo <chestozo@yandex-team.ru>
2014-01-17 12:55:18 +0400

    off test #2

--- tests/no.events.js
+++ tests/no.events.js
@@ -69,13 +69,27 @@ describe('off', function() {
 
         foo.on('foo', handler);
         foo.trigger('foo');
+        expect(count).to.be(1);
+
         foo.off('foo', handler);
         foo.trigger('foo');
-
         expect(count).to.be(1);
     });
 
-    it('remove all handlers of specific type');
+    it('remove all handlers of specific type', function() {
+        var foo = no.extend( {}, no.Events );
+        var calls = '';
+
+        foo.on('foo', function() { calls += 'a'; });
+        foo.on('foo', function() { calls += 'b'; });
+
+        foo.trigger('foo');
+        expect(calls).to.be('ab');
+
+        foo.off('foo');
+        foo.trigger('foo');
+        expect(calls).to.be('ab');
+    });
 
     it('remove all event handles');
 
