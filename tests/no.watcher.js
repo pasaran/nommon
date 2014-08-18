296f7c2edbadaaf2e28cf41af35cfe51815bee6c
Sergei Nikitin <nik.pasaran@gmail.com>
2014-08-18 16:48:58 +0400

    Watcher больше не глобальный объект. +Какие-то тесты

---
+++ tests/no.watcher.js
@@ -0,0 +1,121 @@
+var no = require( '../lib/no.watcher.js' );
+
+var expect = require( 'expect.js' );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+it( 'get/set', function() {
+    var watcher = new no.Watcher();
+
+    watcher.add( 'foo', {
+        value: 42
+    } );
+
+    expect( watcher.get( 'foo' ) ).to.be( 42 );
+
+    watcher.set( 'foo', 24 );
+    expect( watcher.get( 'foo' ) ).to.be( 24 );
+} );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+it( 'change:foo', function() {
+    var watcher = new no.Watcher();
+
+    var n = 0;
+    watcher.on( 'change:foo', function() {
+        n++;
+    } );
+
+    watcher.add( 'foo', {
+        value: 42
+    } );
+
+    watcher.once( 'change:foo', function( e, params ) {
+        expect( params.value ).to.be( 24 );
+    } );
+    watcher.set( 'foo', 24 );
+    watcher.set( 'foo', 24 );
+    watcher.set( 'foo', 24 );
+
+    watcher.once( 'change:foo', function( e, params ) {
+        expect( params.value ).to.be( 42 );
+    } );
+    watcher.set( 'foo', 42 );
+
+    expect( n ).to.be( 2 );
+} );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+it( 'change and multiple set', function() {
+    var watcher = new no.Watcher();
+
+    watcher.add( 'foo', {
+        value: 42
+    } );
+    watcher.add( 'bar', {
+        value: 24
+    } );
+
+    watcher.once( 'change', function( e, params ) {
+        expect( params ).to.be.eql( {
+            foo: {
+                value: 77,
+                old_value: 42
+            }
+        } );
+    } );
+    watcher.set( 'foo', 77 );
+
+    watcher.once( 'change', function( e, params ) {
+        expect( params ).to.be.eql( {
+            foo: {
+                value: 42,
+                old_value: 77
+            },
+            bar: {
+                value: 36,
+                old_value: 24
+            }
+        } );
+    } );
+    watcher.set( {
+        foo: 42,
+        bar: 36
+    } );
+} );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+it( 'computed values', function() {
+    var watcher = new no.Watcher();
+
+    watcher.add( 'foo', {
+        value: 42
+    } );
+    watcher.add( 'bar', {
+        value: 24
+    } );
+
+    watcher.add( 'quu', {
+        expr: 'foo + bar',
+        deps: [ 'foo', 'bar' ]
+    } );
+    expect( watcher.get( 'quu' ) ).to.be( 66 );
+
+    watcher.set( {
+        foo: 36,
+        bar: 22
+    } );
+    watcher.once( 'change:quu', function( e, params ) {
+        expect( params ).to.be.eql( {
+            value: 58,
+            old_value: 66
+        } );
+    } );
+    expect( watcher.get( 'quu' ) ).to.be( 58 );
+} );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
