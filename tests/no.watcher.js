33dee67d02a37044449e46030694b94f0e5f69e2
Sergei Nikitin <nik.pasaran@gmail.com>
2014-08-18 18:23:08 +0400

    Правки и тесты no.watcher

--- tests/no.watcher.js
+++ tests/no.watcher.js
@@ -7,9 +7,7 @@ var expect = require( 'expect.js' );
 it( 'get/set', function() {
     var watcher = new no.Watcher();
 
-    watcher.add( 'foo', {
-        value: 42
-    } );
+    watcher.add( 'foo', { value: 42 } );
 
     expect( watcher.get( 'foo' ) ).to.be( 42 );
 
@@ -27,9 +25,7 @@ it( 'change:foo', function() {
         n++;
     } );
 
-    watcher.add( 'foo', {
-        value: 42
-    } );
+    watcher.add( 'foo', { value: 42 } );
 
     watcher.once( 'change:foo', function( e, params ) {
         expect( params.value ).to.be( 24 );
@@ -51,12 +47,8 @@ it( 'change:foo', function() {
 it( 'change and multiple set', function() {
     var watcher = new no.Watcher();
 
-    watcher.add( 'foo', {
-        value: 42
-    } );
-    watcher.add( 'bar', {
-        value: 24
-    } );
+    watcher.add( 'foo', { value: 42 } );
+    watcher.add( 'bar', { value: 24 } );
 
     watcher.once( 'change', function( e, params ) {
         expect( params ).to.be.eql( {
@@ -91,12 +83,8 @@ it( 'change and multiple set', function() {
 it( 'computed values', function() {
     var watcher = new no.Watcher();
 
-    watcher.add( 'foo', {
-        value: 42
-    } );
-    watcher.add( 'bar', {
-        value: 24
-    } );
+    watcher.add( 'foo', { value: 42 } );
+    watcher.add( 'bar', { value: 24 } );
 
     watcher.add( 'quu', {
         expr: 'foo + bar',
@@ -104,17 +92,43 @@ it( 'computed values', function() {
     } );
     expect( watcher.get( 'quu' ) ).to.be( 66 );
 
-    watcher.set( {
-        foo: 36,
-        bar: 22
-    } );
     watcher.once( 'change:quu', function( e, params ) {
         expect( params ).to.be.eql( {
             value: 58,
             old_value: 66
         } );
     } );
+    watcher.set( {
+        foo: 36,
+        bar: 22
+    } );
     expect( watcher.get( 'quu' ) ).to.be( 58 );
+} );
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+it( 'no changes', function() {
+    var watcher = new no.Watcher();
+
+    watcher.add( 'foo', { value: 42 } );
+    watcher.add( 'bar', { value: 24 } );
+
+    var n = 0;
+    watcher.on( 'change', function() {
+        n++;
+    } );
+
+    watcher.set( 'foo', 42 );
+    watcher.set( 'bar', 24 );
+    watcher.set( {
+        foo: 42
+    } );
+    watcher.set( {
+        foo: 42,
+        bar: 24
+    } );
+
+    expect( n ).to.be( 0 );
 } );
 
 //  ---------------------------------------------------------------------------------------------------------------  //
