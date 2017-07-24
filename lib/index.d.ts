568adf8ca2811bd47dcf7403c220f779d6a99b2a
Sergei Nikitin <nik.pasaran@gmail.com>
2017-07-24 19:08:08 +0300

    version -> 0.0.48

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -26,3 +26,18 @@ export namespace number {
 
 }
 
+type EventsCallback = ( name: string, ...values: Array<any> ): void;
+
+export class Events {
+
+    on( name: string, callback: EventsCallback ): this;
+    off( name: string, callback: EventsCallback ): this;
+    once( name: string, callback: EventsCallback ): this;
+
+    trigger( name: string, ...values: Array<any> ): this;
+    atrigger( name: string, ...values: Array<any> ): void;
+
+    forward( name: string, obj: Events ): this;
+
+}
+
